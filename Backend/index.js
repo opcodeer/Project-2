c92644f47aa682c82ab749729018516ef7dc7037
gautam gautam <gautamdtua42003@gmail.com>
2024-06-26 19:45:26 +0700

    creating build of the project

--- Backend/index.js
+++ Backend/index.js
@@ -2,15 +2,19 @@ import dotenv from 'dotenv';
 import connectToMongo from './db.js'; // Assuming db.js exports connectToMongo as default
 import express from 'express';
 import cors from 'cors';
+import path from 'path';
 dotenv.config();
 connectToMongo(); // Connect to MongoDB
 
 const app = express();
-const port = process.env.PORT | 5000;
+const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, "/build")));
 
 // Routes
 import authRoutes from './routes/auth.js';
@@ -18,6 +22,11 @@ import notesRoutes from './routes/notes.js';
 
 app.use('/api/auth', authRoutes);
 app.use('/api/notes', notesRoutes);
+
+// Serve React App
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 app.listen(port, () => {
   console.log(`iNoteBook backend listening on port ${port}`);
