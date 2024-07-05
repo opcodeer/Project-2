e7693e418df0c977019ccd6b000be18b047b4394
gautam gautam <gautamdtua42003@gmail.com>
2024-07-05 17:27:46 +0700

    Refactoring backend code

--- Backend/index.js
+++ Backend/index.js
@@ -10,7 +10,10 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: '*', // Replace with your frontend domain
+  credentials: true
+}));
 app.use(express.json());
 
 const __dirname = path.resolve();
