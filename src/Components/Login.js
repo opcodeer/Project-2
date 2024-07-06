77ad12d2a0c562c2e4c7d5da2b9443c0366dcfd3
gautam gautam <gautamdtua42003@gmail.com>
2024-07-06 17:01:38 +0700

    refactoring site for deploy

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -8,7 +8,7 @@ const Login = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const response = await fetch("http://localhost:5000/api/auth/login", {
+            const response = await fetch("https://notehub-8fnl.onrender.com/api/auth/login", {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -31,7 +31,7 @@ const Signup = () => {
         e.preventDefault();
         const { name, email, password } = credentials;
         try {
-            const response = await fetch("http://localhost:5000/api/auth/createuser", {
+            const response = await fetch("https://notehub-8fnl.onrender.com/api/auth/createuser", {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
